79d01c2823444d771e44cbbd66f25086cd314979
jaichandar <jaichandarmuthamizh@gmail.com>
2024-10-09 11:36:50 +0530

    router changes

--- index.js
+++ index.js
@@ -9,34 +9,7 @@ const PORT = 5000;
 app.use(cors());
 app.use(morgan('dev'));
 
-app.get("/contries", async(req, res) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    const pageUrl = 'https://www.scrapethissite.com/pages/simple/';
-
-    await page.goto(pageUrl);
-
-    const countries = await page.evaluate(() => {
-        const countryList = document.querySelectorAll('.country');
-        return Array.from(countryList).map((country) => {
-            const countryName = country.querySelector('.country-name').textContent.split('\n').map(item => item.trim()).filter(item => item).join('');
-            const capital = country.querySelector('.country-info .country-capital').textContent;
-            const population = country.querySelector('.country-info .country-population').textContent;
-            const area = country.querySelector('.country-info .country-area').textContent;
-            return {
-                countryName,
-                capital,
-                population,
-                area,
-            }
-        })
-    })
-    res.status(200).json({
-        success: true,
-        data: countries
-    })
-})
+app.use('/api/v1', require('./routes/country'));
 
 app.listen(PORT, () => {
     console.log(`Application Running on PORT: ${PORT}`);
